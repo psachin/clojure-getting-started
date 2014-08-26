089a4241949642018dc5354d4b1ab4b11abc70a9
psachin <iclcoolster@gmail.com>
2014-08-26 10:59:57 +0530

    Defined config variables.

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -7,10 +7,16 @@
             [environ.core :refer [env]]
             [camel-snake-kebab.core :as kebab]))
 
+;;; defining variables
+(def sample (env :sample "sample-string-thing"))
+
 (defn splash []
   {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body (pr-str ["Hello" :from 'Heroku])})
+   :headers {"Content-Type" "text/html"}
+   :body (pr-str ["Hello" :from 'Heroku])
+   :body (for [kind ["camel" "snake" "kebab"]]
+           (format "<a href=\"/%s?input=%s\">%s %s</a><br />"
+                   kind sample kind sample))})
 
 (defroutes app
   (GET "/camel" {{input :input} :params}
